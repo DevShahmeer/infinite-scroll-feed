77c2390f813e9b7d572e39fc17a9897b5901f7b4
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-26 21:08:32 +0500

    Main component for the Infinite Scroll Feed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import PostList from './components/PostList';
 
 function App() {
-
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>Infinite Scroll Feed</h1>
+      </header>
+      <main>
+        <PostList />
+      </main>
+      <footer className="App-footer">
+        <p>Scroll down to load more posts...</p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

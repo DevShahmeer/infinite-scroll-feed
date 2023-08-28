2840244b2960b89b9c9613ff7d6abad253ae525e
Devshahmeer <theshahmeerfsd@gmail.com>
2023-08-28 22:43:00 +0500

    Implemented scroll-to-top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import PostList from './components/PostList';
 
 function App() {
+  const [ showScrollButton, setShowScrollButton ] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShowScrollButton(true);
+    } else {
+      setShowScrollButton(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo( { top: 0, behavior: 'smooth' })
+  }
+
+  // Attach the scroll event listener
+  window.addEventListener('scroll', handleScroll);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +31,11 @@ function App() {
       <footer className="App-footer">
         <p>Scroll down to load more posts...</p>
       </footer>
+      {showScrollButton && (
+        <button className="scroll-button" onClick={scrollToTop}>
+        ↑
+      </button>
+      )}
     </div>
   );
 }
